a164eb0282ad2c460ca29e8812e79e5e8cbecbdc
Arjit Chaudhary <arjit.c@gmail.com>
2014-05-08 21:51:42 +0530

    Update install-mongodb-centos.sh

--- install-mongodb-centos.sh
+++ install-mongodb-centos.sh
@@ -4,16 +4,20 @@
 
 clear
 echo 'Starting MongoDB install'
-
-echo '[10gen]' >> /etc/yum.repos.d/10gen.repo
-echo 'name=10gen Repository' >> /etc/yum.repos.d/10gen.repo
-echo 'baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64' >> /etc/yum.repos.d/10gen.repo
-echo 'gpgcheck=0' >> /etc/yum.repos.d/10gen.repo
-echo 'enabled=1' >> /etc/yum.repos.d/10gen.repo
-
-yum install -y mongo-10gen mongo-10gen-server
-
-service mongod start
-chkconfig mongod on
-clear
+echo 'NOTE! This is for 64BIT ONLY!'
+read -p "Are you sure you want to continue? (y/n) " RESP
+if [ "$RESP" = "y" ]; then
+  echo '[10gen]' >> /etc/yum.repos.d/10gen.repo
+  echo 'name=10gen Repository' >> /etc/yum.repos.d/10gen.repo
+  echo 'baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64' >> /etc/yum.repos.d/10gen.repo
+  echo 'gpgcheck=0' >> /etc/yum.repos.d/10gen.repo
+  echo 'enabled=1' >> /etc/yum.repos.d/10gen.repo
+  yum install -y mongo-10gen mongo-10gen-server
+  service mongod start
+  chkconfig mongod on
+  clear
 echo 'MongoDB is installed and running.'
+else
+  echo "Ok, MongoDB wasn't installed"
+fi
+
